1021e41dc1c939a0bb5a1485b4ec9ee9370512de
CJACKSON61 <31939904+CJACKSON61@users.noreply.github.com>
2017-12-13 14:39:26 -0800

    Final Version

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var SerialPort = require ('serialport');
+const mqtt = require('mqtt');
+const client = mqtt.connect('mqtt://10.10.47.203');
+//var SerialPort = require ('serialport');
 //import express
 var express = require('express');
 //create express object named app
@@ -6,6 +8,7 @@ var app = express();
 
 //instantiate a server on port 3030
 var server = app.listen(process.env.PORT || 3030);
+
 var io = require('socket.io')(server);
 
 /*var serialPort = new SerialPort("/dev/cu.usbmodemFA131", {
@@ -19,6 +22,20 @@ app.use(express.static('public'));
 //on a request to / serve index.html
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+
+client.on('connect', function () {
+  client.subscribe('/mytopic');
+  client.subscribe('/tft');
+  client.subscribe('/clock');
+  client.subscribe('/fan');
+  client.publish('/mytopic', 'Hello mqtt');
+});
+
+client.on('message', function(topic, message){
+    console.log(message.toString());
+    io.sockets.emit('mqtt',{'topic':String(topic),
+    'message':String(message)});
 });
 
 /*serialPort.on('open', function(){
